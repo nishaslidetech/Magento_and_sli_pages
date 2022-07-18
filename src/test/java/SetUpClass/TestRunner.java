fd987495ae87f67a82a2b694df55a91362e63118
nishaslidetech <nisha.dhiman@slidetech.in>
2022-07-18 18:27:28 +0530

    update code

--- src/test/java/SetUpClass/TestRunner.java
+++ src/test/java/SetUpClass/TestRunner.java
@@ -9,9 +9,9 @@ import io.cucumber.junit.CucumberOptions;
 
 //@RunWith(Cucumber.class)
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "." }, glue = { "stepDefination" }, //tags = ("@Sli_pagination_test" ), plugin = {
-		"pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json", "usage:target/usage.jsonx",
-		"junit:target/cucumber.xml" })
+@CucumberOptions(features = { "." }, glue = { "stepDefination" }, // tags = ("@Sli_pagination_test" ),
+		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber/cucumber.json",
+				"usage:target/usage.jsonx", "junit:target/cucumber.xml" })
 
 public class TestRunner {
 
